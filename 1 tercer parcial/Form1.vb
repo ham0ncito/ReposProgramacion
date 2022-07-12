551417e0a14eb58d74c42c6091e4c9cbd4e5ce11
ham0ncito <lil_ham0n@hotmail.com>
2022-07-12 13:59:11 -0600

    suo

--- 1 tercer parcial/Form1.vb
+++ 1 tercer parcial/Form1.vb
@@ -1,3 +1,9 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim con As New SqlConnection(My.Settings.conexion)
+        Dim sql As String = "Select idProducto, nombreProducto, fecha from Productos"
+        Dim com As New SqlCommand(sql, con)
 
+    End Sub
 End Class
